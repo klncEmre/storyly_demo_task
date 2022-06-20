271f64055d6d82d2982cf7eacf9f741ff59ee1eb
klncEmre <kilince18@itu.edu.tr>
2022-06-20 14:23:29 +0300

    factory implemented

--- storyly_demo_task/ViewController.swift
+++ storyly_demo_task/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import Storyly
 
 class ViewController: UIViewController {
+    private let emresview: StoryGroupViewFactory = SampleViewFactory()
+    
     let screenSize: CGRect = UIScreen.main.bounds
     let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
     var initialLoad: Bool = true
@@ -26,11 +28,12 @@ class ViewController: UIViewController {
         containerView.translatesAutoresizingMaskIntoConstraints = false
         containerView.widthAnchor.constraint(equalToConstant: screenSize.width).isActive = true
 //        containerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant:20).isActive = true
-        self.containerView.delegate = self //Set up delegate
-        self.containerView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        self.containerView.delegate = self //Set up delegat
         containerView.storyGroupTextColor = UIColor.systemBlue
         containerView.storyGroupIconBackgroundColor = UIColor.yellow
+        self.containerView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         containerView.rootViewController = self
+        containerView.storyGroupViewFactory = emresview
         
     }
 }
@@ -44,6 +47,7 @@ extension ViewController: StorylyDelegate {
             storylyView.isHidden = false
         }
         initialLoad  = true
+        print("-----" + String(storyGroupList.count))
         
     }
     func storylyLoadFailed(_ storylyView: Storyly.StorylyView,
@@ -55,3 +59,41 @@ extension ViewController: StorylyDelegate {
 }
 
 
+class SampleViewFactory: StoryGroupViewFactory {
+
+    func createView() -> StoryGroupView {
+        return SampleView(frame: .zero)
+    }
+
+    // Size of your custom view
+    func getSize() -> CGSize {
+        return CGSize(width: 10, height: 178)
+    }
+}
+class SampleView: StoryGroupView {
+    let kCONTENT_XIB_NAME = "SampleView"
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var sampleText: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed(kCONTENT_XIB_NAME, owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+    }
+    
+    override func populateView(storyGroup: StoryGroup?) {
+        // Handle populating view with Storyly related data
+        // ie, self.sampleText.text = storyGroup?.title
+    }
+}
