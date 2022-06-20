66d360cc662f8b83953fc2717d37e1e344cd6ec1
klncEmre <kilince18@itu.edu.tr>
2022-06-20 09:13:34 +0300

    Initial Commit

---
+++ storyly_demo_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyly_demo_task
+//
+//  Created by Emre Kılınç on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
